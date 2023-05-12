3d362065c9f6b8456fced05010469cab5597a7ce
leninrkb <rkb.lenin@gmail.com>
2023-05-11 23:50:59 -0500

    probando  ts

---
+++ componentes-ionic/src/components/Carnes.vue
@@ -0,0 +1,40 @@
+<template>
+    <h2 class="text-2xl font-semibold
+    mb-5">
+        Esta es la seccion de carnes
+    </h2>
+    <div class="flex flex-wrap gap-3">
+        <div class="bg-cyan-100 p-1 rounded-lg w-1/3
+        transition ease-in-out duration-100
+        hover:shadow-lg hover:bg-cyan-300 hover:cursor-pointer">
+            <h3 class="text-md font-semibold italic">
+                Carne de res
+            </h3>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur alias quos veritatis minus non! Nobis veniam velit quaerat, earum, fuga placeat quod minima ducimus facere, quisquam labore corporis excepturi ullam?
+            </p>
+        </div>
+        <div class="bg-cyan-100 p-1 rounded-lg w-1/3
+        hover:animate-bounce">
+            <h3 class="text-md font-semibold italic">
+                Carne de cerdo
+            </h3>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur alias quos veritatis minus non! Nobis veniam velit quaerat, earum, fuga placeat quod minima ducimus facere, quisquam labore corporis excepturi ullam?
+            </p>
+        </div>
+        <div class="bg-cyan-100 p-1 rounded-lg w-1/3
+        hover:animate-pulse">
+            <h3 class="text-md font-semibold italic">
+                Carne de pollo
+            </h3>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur alias quos veritatis minus non! Nobis veniam velit quaerat, earum, fuga placeat quod minima ducimus facere, quisquam labore corporis excepturi ullam?
+            </p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+export default class Carnes extends Vue { }
+</script>
--- componentes-ionic/src/views/HomeView.vue
+++ componentes-ionic/src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header class="w-fit mx-auto
+    text-3xl font-semibold font-sans mt-10 mb-10
+    shadow-lg rounded-lg p-3
+    transition ease-in-out duration-500
+    hover:bg-amber-500 hover:text-gray-100
+    hover:shadow-amber-500">
+    Cosas que no puedo comer :(
+  </header>
+  <nav class="container mx-auto
+  bg-cyan-500 p-1 rounded-lg
+  text-gray-100 text-lg font-semibold">
+    <ul class="flex gap-5">
+      <li @click="navegar('carnes')" class="p-2 rounded-lg
+      transition ease-in-out duration-300 
+      hover:-translate-y-1
+      hover:underline underline-offset-4
+      hover:shadow-lg
+      cursor-pointer">Carnes</li>
+      <li class="p-2 rounded-lg
+      transition ease-in-out duration-300 
+      hover:-translate-y-1
+      hover:underline underline-offset-4
+      hover:shadow-lg
+      cursor-pointer">Liquidos</li>
+      <li class="p-2 rounded-lg
+      transition ease-in-out duration-300 
+      hover:-translate-y-1
+      hover:underline underline-offset-4
+      hover:shadow-lg
+      cursor-pointer">Harinas</li>
+    </ul>
+  </nav>
+  <div class="container mx-auto">
+    <div class="h-1 bg-cyan-500 my-1 rounded-full"></div>
+    <div class="">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
+import { defineComponent } from 'vue'
+export default defineComponent({
+  methods: {
+    navegar(path:string){
+      this.$router.push(path)
+    }
   },
 })
-export default class HomeView extends Vue {}
 </script>
