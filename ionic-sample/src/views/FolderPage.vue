0ce8bea4e652d1333cf374ad0ca3d92de82629b4
leninrkb <rkb.lenin@gmail.com>
2023-05-13 23:22:15 -0500

    update folderpage

--- ionic-sample/src/views/FolderPage.vue
+++ ionic-sample/src/views/FolderPage.vue
@@ -1,6 +1,34 @@
+<script lang="ts" setup>
+import { IonPage, IonHeader, IonContent, IonFooter, IonToolbar } from '@ionic/vue';
+</script>
 <template>
-    <div class="text-3xl p-2 bg-teal-500 rounded-lg m-10">
-      texto de prueba
-    </div>
+    <ion-page>
 
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Editor de metadata</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content>
+        <div class="container mx-auto p-3 font-sans">
+          <h1 class="text-3xl">Editar cancion</h1>
+          <div class="rounded-lg bg-gray-600 h-screen p-3">
+            <div class="shadow-md rounded-md w-fit">
+              <button class="p-3 text-lg">
+                Selecciona una cancion
+              </button>
+            </div>
+          </div>
+        </div>
+      </ion-content>
+
+      <ion-footer>
+        <div class="container mx-auto p-1 bg-gradient-to-br from-cyan-600 to-teal-600 font-mono">
+          <div class="w-fit mx-auto">
+            <h6 class="text-gray-800">created by leninrkb - 2023</h6>
+          </div>
+        </div>
+      </ion-footer>
+    </ion-page>
 </template>
